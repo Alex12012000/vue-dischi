4c12b8397013e3dad1144daee72f840bb95a8ed2
Alex12012000 <arcex.29@gmail.com>
2022-07-08 16:01:40 +0200

    header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <header>
+        <div class="logo">
+            <img src="../assets/logo.png" alt="logo">
+        </div>
+    </header>
+
+</template>
+
+
+<script>
+export default {
+    name: 'PageHeader',
+}
+</script>
+
+
+<style lang="scss">
+    header {
+        height: 80px;
+        background-color: #2e3a46;
+
+        .logo {
+            display: flex;
+            height: 100%;
+            align-items: center;
+            margin: 0 20px;
+            img {
+                width: 55px;
+                height: 55px;
+            }
+        }
+    }
+</style>
