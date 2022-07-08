e840fd26d051d8fbc71696cfe69f874f106c0849
Alex12012000 <arcex.29@gmail.com>
2022-07-08 16:04:33 +0200

    Api call

---
+++ src/components/MainContent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="content">
+            <MusicCard v-for="cd, index in cdArray" :key="index" :detail="cd"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import MusicCard from './MusicCard.vue'
+
+export default {
+    name: 'MainContent',
+    components: {
+        MusicCard
+    },
+    data() {
+        return {
+            url: 'https://flynn.boolean.careers/exercises/api/array/music',
+            cdArray: [],
+        }
+    },
+    methods: {
+        getMusicInfo(){
+            axios.get(this.url).then(response => {
+                this.cdArray = response.data.response;
+            })
+        }
+    },
+    mounted() {
+        this.getMusicInfo()
+    } 
+        
+    
+}
+</script>
+
+<style lang="scss">
+    
+</style>
---
+++ src/components/MusicCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <img :src="detail.poster" :alt="detail.title">
+        <h3>{{detail.title}}</h3>
+        <span>{{detail.author}}</span>
+        <span>{{detail.year}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MusicCard',
+    props: {
+        detail: Object
+    }
+}
+</script>
+
+
+<style lang="">
+    
+</style>
