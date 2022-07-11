830f417721b4aa7e205fd6f588dd55b17a8974ef
Alex12012000 <arcex.29@gmail.com>
2022-07-11 17:42:01 +0200

    select styling

--- src/components/SelectComponent.vue
+++ src/components/SelectComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="select-bar">
         <select v-model="selected" @change="$emit('changeValue', selected)">
             <option value="All">All</option>
             <option value="Rock">Rock</option>
@@ -21,6 +21,19 @@ export default {
 }
 </script>
 
-<style lang="scss">
-    
+<style lang="scss" scoped>
+    .select-bar {
+        padding-top: 20px;
+        text-align: center;
+        select {
+            padding: 5px 0;
+            padding-right: 20px;
+            padding-left: 5px;
+            border-radius: 10px;
+            border: 1px solid #2e3a46;
+            text-align: left;
+            background-color: #1e2d3b;
+            color: white;
+        }
+    }
 </style>
